6ed1c7d4ae7ab47824b9e459b34e716955d96658
NSamochvalovas <samochvalovas.neimantas@gmail.com>
2022-06-21 20:58:54 +0300

    btn hover added

--- src/components/CategoryItem/CategoryItems.styles.jsx
+++ src/components/CategoryItem/CategoryItems.styles.jsx
@@ -31,4 +31,10 @@ export const Button = styled.button`
   border-radius: 5px;
   color:#fff;
   padding:10px 20px;
+  &:hover{
+    color:#000;
+    background: #fff;
+    font-weight:bold;
+    transition: 0.5s ease;
+  }
 `;
--- src/components/Slider/Slider.styles.jsx
+++ src/components/Slider/Slider.styles.jsx
@@ -62,6 +62,12 @@ export const Button = styled.button`
   border-radius: 5px;
   color:#fff;
   padding:10px 20px;
+  &:hover{
+    color:#000;
+    background: #fff;
+    font-weight:bold;
+    transition: 0.5s ease;
+  }
 `;
 
 export const H1 = styled.h1`
